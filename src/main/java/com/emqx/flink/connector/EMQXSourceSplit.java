37a6a6aa1c4b3f9a14e7c626f2ec9c6439a8d1ed
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-09-24 17:36:46 -0300

    refactor: remove unused fields

--- src/main/java/com/emqx/flink/connector/EMQXSourceSplit.java
+++ src/main/java/com/emqx/flink/connector/EMQXSourceSplit.java
@@ -1,47 +1,26 @@
 package com.emqx.flink.connector;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import org.apache.flink.api.connector.source.SourceSplit;
-
-import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 
 class EMQXSourceSplit implements SourceSplit, Serializable {
     private String clientid;
 
-    private List<Mqtt5Publish> messagesToCheckpoint;
-
     EMQXSourceSplit(String clientid) {
-        this(clientid, Collections.emptyList());
-    }
-
-    EMQXSourceSplit(String clientid, List<Mqtt5Publish> messagesToCheckpoint) {
         this.clientid = clientid;
-        this.messagesToCheckpoint = messagesToCheckpoint;
-    }
-
-    public void putMsgs(List<Mqtt5Publish> messagesToCheckpoint) {
-        this.messagesToCheckpoint = new ArrayList<>(messagesToCheckpoint);
     }
 
     public String getClientid() {
         return this.clientid;
     }
 
-    public List<Mqtt5Publish> getMsgs() {
-        List<Mqtt5Publish> copy = new ArrayList<>(this.messagesToCheckpoint);
-        return copy;
-    }
-
     public String splitId() {
-        return clientid;
+        return this.clientid;
     }
 
     @Override
     public String toString() {
-        return String.format("EMQXSourceSplit(%s)[%s]", clientid, messagesToCheckpoint);
+        return String.format("EMQXSourceSplit(%s)", clientid);
     }
 }
